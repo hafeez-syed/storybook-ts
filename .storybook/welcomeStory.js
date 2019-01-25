1a346f73a4b2d41d99558a7ee819b34f1a663402
Hafeez Syed <hafeez.syed@nintex.com>
2019-01-25 13:56:35 +1100

    welcome page added

---
+++ .storybook/welcomeStory.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("Welcome", module).add("to my new storybook 🎉", () => (
+  <div>Welcome to my new Storybook 🎉 </div>
+));
