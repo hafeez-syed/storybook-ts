a572287d22f8fcc8222e64ad9e60570a14f39dbd
Hafeez Syed <hafeez.syed@nintex.com>
2019-01-25 15:26:26 +1100

    storybook addons added

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -4,5 +4,9 @@ import { storiesOf } from "@storybook/react";
 import { Button } from "./Button";
 
 storiesOf("Button", module)
-  .add("with background", () => <Button bg="plaegoldenrod">Hello world</Button>)
-  .add("with green background", () => <Button bg="green">Hello green</Button>);
+  .addWithJSX("with background", () => (
+    <Button bg="palegoldenrod">Hello world</Button>
+  ))
+  .addWithJSX("with green background", () => (
+    <Button bg="green">Hello green</Button>
+  ));
