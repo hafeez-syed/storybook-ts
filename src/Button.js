4a1de1cd5acdb663609d44e8dc9b45be6b3c18f7
Hafeez Syed <hafeez.syed@nintex.com>
2019-01-25 16:33:53 +1100

    typescript added to react jsx

--- src/Button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Button = ({ bg, children }) => {
-  return <button style={{ background: bg }}>{children}</button>;
-};
--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { color, text } from "@storybook/addon-knobs/react";
+import { boolean, color, text } from "@storybook/addon-knobs/react";
 
 import { Button } from "./Button";
 import { wInfo } from "./utils";
@@ -27,4 +27,19 @@ storiesOf("Button", module)
     ~~~
     
     `)(() => <Button bg={color("bg", "green", "group1")}>Hello green</Button>)
+  )
+  .addWithJSX(
+    "with disabled 'false'",
+    wInfo(`
+    description
+    
+    ~~~js
+    <Button>Click here</Button>
+    ~~~
+    
+    `)(() => (
+      <Button disabled={boolean("disabled", true)}>
+        {text("text", "Hello world!")}
+      </Button>
+    ))
   );
